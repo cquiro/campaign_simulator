70a3d66408c01a7d39e96c2d009e5c8cc9decb03
Carlos Quiroga <cquiro@gmail.com>
2015-09-01 10:23:53 -0500

    Modifies decription of program and cleans code

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # campaign_simulator
-This is a Ruby program that simulates a campaign. You can create politicians and voters, list them, update 
-them one at a time and simulate the voting. The outcome depends on the politics of the voters, so if most of
-them are liberal and socialist, then the elected official would most likely be a democrat.
+This is a Ruby program that simulates a campaign. You can create politicians and voters, list them, update
+them one at a time and simulate the voting. The outcome depends on the politics of the voters. For instance if most of
+the voters are liberal and socialist, then the elected official would most likely be a democrat.
+
+This program is a REPL ruby program.
--- campaign.rb
+++ campaign.rb
@@ -1,10 +1,6 @@
-#campaign.rb
-
 system "clear"
 
-
 module Prompt
-	
 	#method to ask a question and get a response from the keyboard. Answer options may or may not be included.
 	def ask_for_options(question, options = [])
 		if options.length > 0
@@ -23,14 +19,10 @@ module Prompt
 	def separator
 		puts "*"*100
 	end
-
 end
-
-
 
 #definition of person class.
 class Person
-
 	attr_accessor :name, :party, :politician, :politics
 
 	def initialize(name, politics)
@@ -47,14 +39,10 @@ class Person
 	def to_s
 		@name
 	end
-
 end
-
-
 
 #definition of politician class.
 class Politician < Person
-	
 	attr_accessor :name, :party, :politician, :politics, :votes
 
 	def initialize(name, party)
@@ -64,14 +52,11 @@ class Politician < Person
 		@politician = "yes"
 		@votes = 1
 	end
-
 end
-
 
 #definition of class campaign and its methods
 class Campaign
-	
-	include Prompt 
+	include Prompt
 
 	def initialize
 		@characters = []
@@ -79,29 +64,26 @@ class Campaign
 
 	#method to display and "navigate" the main menu.
 	def display_main_menu
-
-	quit = "no"
+		quit = "no"
 
 		while quit == "no" do
-
 			begin
-	
 			main_options = ["create", "list", "update", "vote", "leave"]
 			main_option = ask_for_options("What would you like to do? -- Create, List, Update, Vote or Leave --", main_options)
 
 				case main_option
-					when "create"
-						create
-		 			when "list"
-		 				list_characters
-		 			when "update"
-		 				update
-		 			when "vote"
-		 				vote
-		 			when "leave"
-		 				quit = ask_for_options("Are you sure you want to leave? -- yes or no --", ["yes", "no"])
-		 			else
-		 				puts "Please type in one of the options prompted."
+				when "create"
+					create
+	 			when "list"
+	 				list_characters
+	 			when "update"
+	 				update
+	 			when "vote"
+	 				vote
+	 			when "leave"
+	 				quit = ask_for_options("Are you sure you want to leave? -- yes or no --", ["yes", "no"])
+	 			else
+	 				puts "Please type in one of the options prompted."
 				end
 
 			end until main_options.include? main_option
@@ -110,54 +92,47 @@ class Campaign
 
 	#method to create characters for this campaign with the role of politician or person(voter)
 	def create
-		
-	system "clear"
+		system "clear"
 
 		begin
+			@human_options = ["politician", "person"]
+			@party_options = ["republican", "democrat"]
+			@politics_options = ["liberal", "conservative", "tea party", "socialist", "neutral"]
 
-		@human_options = ["politician", "person"]
-		@party_options = ["republican", "democrat"]
-		@politics_options = ["liberal", "conservative", "tea party", "socialist", "neutral"]
-		
-		human = ask_for_options("What would you like to create? -- Politician or Person --", @human_options)
+			human = ask_for_options("What would you like to create? -- Politician or Person --", @human_options)
 
 			if human == "politician"
-		  	
-		  		pol_name = ask_for_options("Please enter the name of the politician.")
-		  		party = ask_for_options("Please enter the party this new politician represents. -- Republican or Democrat --", @party_options)
-		  		politician = Politician.new(pol_name, party)
-		  		@characters << politician
-
+	  		pol_name = ask_for_options("Please enter the name of the politician.")
+	  		party = ask_for_options("Please enter the party this new politician represents. -- Republican or Democrat --", @party_options)
+	  		politician = Politician.new(pol_name, party)
+	  		@characters << politician
 			elsif human == "person"
-		  		
-		  		per_name = ask_for_options("Please enter the name of the person.")
-		  		politics = ask_for_options("Please enter the political affiliation of this person. -- Liberal, Conservative, Tea Party, Socialist, or Neutral --", @politics_options)
-		  		person = Person.new(per_name, politics)
-		  		@characters << person
-		  	
-		  	else
-		  		puts "Please type in one of the options prompted."
+	  		per_name = ask_for_options("Please enter the name of the person.")
+	  		politics = ask_for_options("Please enter the political affiliation of this person. -- Liberal, Conservative, Tea Party, Socialist, or Neutral --", @politics_options)
+	  		person = Person.new(per_name, politics)
+	  		@characters << person
+		  else
+		  	puts "Please type in one of the options prompted."
 		 	end
 
-		 	puts "A new character has been created with a role of #{human}."
+			puts "A new character has been created with a role of #{human}."
 
-		 end until @human_options.include? human
+		end until @human_options.include? human
 	end
 
 	#method to list the characters created for this campaign.
 	def list_characters
-		
 		system("clear")
 		separator
+
 		format = "%-25s %-25s %-25s %s"
 		puts format % ["Role", "Name", "Party", "Politics"]
-		
+
 		@characters.each do |character|
-  			
-  			if character.politician == "yes"
-  				puts format % ["Politician", character.name.capitalize, character.party.capitalize, character.politics.capitalize]
-  			else
-  				puts format % ["Voter", character.name.capitalize, character.party.capitalize, character.politics.capitalize]				
+			if character.politician == "yes"
+				puts format % ["Politician", character.name.capitalize, character.party.capitalize, character.politics.capitalize]
+			else
+				puts format % ["Voter", character.name.capitalize, character.party.capitalize, character.politics.capitalize]
 			end
 		end
 		separator
@@ -165,47 +140,39 @@ class Campaign
 
 	#method to update the characters on this campaign.
 	def update
-		
 		old_name = ask_for_options("Please enter the name of the character you would like to update.")
 		index_update = -1 #this index does not change its value unless a match for the name is found in the array of characters.
 
 		@characters.each_with_index do |character, i|
-  			if character.name == old_name
-  				index_update = i #index_update is set to the index of the character matching the search.
-  			end
-  		end
+			if character.name == old_name
+				index_update = i #index_update is set to the index of the character matching the search.
+			end
+  	end
 
-  		if index_update >= 0 #if the index is >= 0 is because a match was found.
-  			
-  			new_name = ask_for_options("Please enter the new name for this character.")
-  			@characters[index_update].name = new_name
+		if index_update >= 0 #if the index is >= 0 is because a match was found.
+			new_name = ask_for_options("Please enter the new name for this character.")
+			@characters[index_update].name = new_name
 
-  			if @characters[index_update].politician == "yes"
-  				new_party = ask_for_options("Please enter the new party for this character. -- Republican or Democrat --", @party_options)
-  				@characters[index_update].party = new_party
-  			else
-  				new_politics = ask_for_options("Please enter the new politics of this character. -- Liberal, Conservative, Tea Party, Socialist, or Neutral --", @politics_options)
-  				@characters[index_update].politics = new_politics
-  			end
-  		
-  		else
-
+			if @characters[index_update].politician == "yes"
+				new_party = ask_for_options("Please enter the new party for this character. -- Republican or Democrat --", @party_options)
+				@characters[index_update].party = new_party
+			else
+				new_politics = ask_for_options("Please enter the new politics of this character. -- Liberal, Conservative, Tea Party, Socialist, or Neutral --", @politics_options)
+				@characters[index_update].politics = new_politics
+			end
+		else
 			puts "That character is not part of the current campaign."
-  		
-  		end
-	end 
-
-
+  	end
+	end
 
 	def vote
-
-	system "clear"
+		system "clear"
 
 		politicians = []
 		persons = []
 
-		@characters.each do |character| 
-			if character.politician == "yes" 
+		@characters.each do |character|
+			if character.politician == "yes"
 				politicians << character
 			else
 				persons << character
@@ -213,51 +180,51 @@ class Campaign
 		end
 
 		#The code below assigns new votes randomly to candidates depending on the politics of the voter
-		#and the party of the candidate. According to that relationship there are different weights 
+		#and the party of the candidate. According to that relationship there are different weights
 		#each vote has. It does not work as a regular voting system as every voter could vote several times
-		#and some times less votes than the total of voters could result, but the overall tendency does 
+		#and some times less votes than the total of voters could result, but the overall tendency does
 		#reflect the weight system and relationship between politics and parties.
 		politicians.each do |poli|
 			if poli.party == "republican"
 				persons.each do |pers|
 					case pers.politics
-						when "tea party"
-							@prob = 0.9
-						when "conservative"
-							@prob = 0.75
-						when "neutral"
-							@prob = 0.5
-						when "liberal"
-							@prob = 0.25
-						when "socialist"
-							@prob = 0.1
+					when "tea party"
+						@prob = 0.9
+					when "conservative"
+						@prob = 0.75
+					when "neutral"
+						@prob = 0.5
+					when "liberal"
+						@prob = 0.25
+					when "socialist"
+						@prob = 0.1
 					end
 					vote = rand < @prob
-					poli.votes += 1 if vote 
+					poli.votes += 1 if vote
 				end
 			else
-				persons.each do |pers|	
+				persons.each do |pers|
 					case pers.politics
-						when "tea party"
-							@prob = 0.1
-						when "conservative"
-							@prob = 0.25
-						when "neutral"
-							@prob = 0.5
-						when "liberal"
-							@prob = 0.75
-						when "socialist"
-							@prob = 0.9
+					when "tea party"
+						@prob = 0.1
+					when "conservative"
+						@prob = 0.25
+					when "neutral"
+						@prob = 0.5
+					when "liberal"
+						@prob = 0.75
+					when "socialist"
+						@prob = 0.9
 					end
 					vote = rand < @prob
-					poli.votes += 1 if vote 
+					poli.votes += 1 if vote
 				end
 			end
 		end
 
 		winner = ""
 		constituence = 0
-		politicians.each do |poli|  
+		politicians.each do |poli|
 			if poli.votes > constituence
 				constituence = poli.votes
 				winner = poli.name
@@ -267,19 +234,11 @@ class Campaign
 		separator
 		puts "The winner of this campaign is #{winner.capitalize}. Better times are ahead of us under our new leader!"
 		separator
-
 		politicians.map { |poli| poli.votes = 1 }
-
 	end
-
-
 end
 
 #starting point of the simulation program.
 puts("\n\n\n" + "*"*20 + " Welcome to a new campaign! " + "*"*20 + "\n\n\n")
 campaign2015 = Campaign.new
 campaign2015.display_main_menu
-
-
-
-
